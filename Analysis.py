224296b26e0c33460dca624ef910754f91332fbb
HungBT <hungbuitien19081997@gmail.com>
2022-10-14 19:54:24 +0700

    sửa lại tệp

---
+++ Analysis.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import uproot
+
+class analysis:
+    
+    __root_file = 
+    
+    def __init__(self):
+        pass
+    
+    def psfAnalysis(self, file_name):
+        root_file = uproot.open(file_name)
+        
+        return root_file
+    
+    
+        
